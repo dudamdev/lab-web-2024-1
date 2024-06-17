67d4b58dd35aa327791aac6dac6141a55313c15d
Eduarda Matos <duda.mdev@gmail.com>
2024-06-17 20:54:19 -0300

    updating author CRUD

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -11,6 +11,40 @@ exports.createAuthor = async (req, res) => {
     }
 };
 
+exports.getAllAuthors = async (req, res) => {
+    try {
+        const authors = await Author.find();
+        res.send(authors);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+
+exports.getAuthorById = async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id);
+        if (!author) {
+            return res.status(404).send();
+        }
+        res.send(author);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+exports.updateAuthor = async (req, res) => {
+    try {
+        const author = await Author.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!author) {
+            return res.status(404).send();
+        }
+        res.send(author);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
 exports.deleteAuthor = async (req, res) => {
     try {
         const books = await Book.find({ author: req.params.id });
