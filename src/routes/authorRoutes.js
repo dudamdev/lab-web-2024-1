a831d0f7e479c710f6367d6fbe379157682d8794
Eduarda Matos <duda.mdev@gmail.com>
2024-06-17 21:24:24 -0300

    updating author routes

--- src/routes/authorRoutes.js
+++ src/routes/authorRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const authorController = require('../controllers/authorController');
 
 router.post('/authors', authorController.createAuthor);
+router.get('/authors', authorController.getAllAuthors);
+router.get('/authors/:id', authorController.getAuthorById);
+router.put('/authors/:id', authorController.updateAuthor);
 router.delete('/authors/:id', authorController.deleteAuthor);
 
 module.exports = router;
